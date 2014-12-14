51ec55ede08392570a4901cd5a414a23b983009a
Lucas Fechio <lucasfmarin@hotmail.com>
2014-12-14 17:31:51 -0200

    Add precompilded assets for Heroku

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,20 +3,20 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :estate, :city, :description, :email, :password, :password_confirmation, :avatar)
+    params.require(:user).permit(:name, :state, :city, :description, :email, :password, :password_confirmation, :avatar)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :estate, :city, :description, :email, :password, :password_confirmation, :current_password, :avatar)
+    params.require(:user).permit(:name, :state, :city, :description, :email, :password, :password_confirmation, :current_password, :avatar)
   end
 
   before_filter :configure_devise_params, if: :devise_controller?
   def configure_devise_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:name, :estate, :city, :description, :email, :password, :password_confirmation, :avatar)
+      u.permit(:name, :state, :city, :description, :email, :password, :password_confirmation, :avatar)
     end
     devise_parameter_sanitizer.for(:edit) do |u|
-      u.permit(:name, :estate, :city, :description, :email, :password, :password_confirmation, :current_password, :avatar)
+      u.permit(:name, :state, :city, :description, :email, :password, :password_confirmation, :current_password, :avatar)
     end
   end
 end
